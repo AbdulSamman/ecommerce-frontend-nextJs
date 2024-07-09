b503e0af153be1ab62105f0b93d1166fbe95167e
AbdulSamman <sammanab@outlook.de>
2024-07-09 03:27:41 +0200

    finish

--- src/_components/Header.tsx
+++ src/_components/Header.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { UserButton, useUser } from "@clerk/nextjs";
 import { CgShoppingCart } from "react-icons/cg";
 import { useEffect, useState } from "react";
