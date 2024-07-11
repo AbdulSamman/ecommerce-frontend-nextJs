914d6a981d3948d6e0585078b904e08b6a36068e
AbdulSamman <sammanab@outlook.de>
2024-07-11 17:01:04 +0200

    test

--- src/_components/ProductList.tsx
+++ src/_components/ProductList.tsx
@@ -9,15 +9,15 @@ const ProductList = () => {
   const { products }: any = useContext(AppContext);
   return (
     <div className=" products gap-3 grid bg-blue grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
-      {products.map((product: IProduct) => {
+      {products?.map((product: IProduct) => {
         return (
           <Link
-            key={product.id}
-            href={`/product-details/${product.id}`}
+            key={product?.id}
+            href={`/product-details/${product?.id}`}
             className="product p-1 border-teal-400 hover:border hover:cursor-pointer rounded-lg">
             <div className="images flex justify-center rounded-t-lg ">
               <Image
-                src={product.attributes.banner.data.attributes.url}
+                src={product?.attributes?.banner?.data?.attributes?.url}
                 alt="bannerCard"
                 width={350}
                 height={210}
@@ -27,18 +27,18 @@ const ProductList = () => {
             </div>
             <div className="py-2 text-start px-1 bg-gray-100 rounded-b-lg ">
               <h2 className="text-[12px] line-clamp-1">
-                {product.attributes.title}
+                {product?.attributes?.title}
               </h2>
 
               <div className="flex justify-between items-center">
                 <div className="py-1 flex items-center gap-x-2 text-gray-500">
                   <FaList />
                   <span className="text-[10px]">
-                    {product.attributes.category}
+                    {product?.attributes?.category}
                   </span>
                 </div>
                 <span className="text-[15px]">
-                  {product.attributes.price} €
+                  {product?.attributes?.price} €
                 </span>
               </div>
             </div>
